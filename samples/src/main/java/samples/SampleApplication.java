dcda1f4cbe6fa570c1a59a19219e81d9efc618cd
lyremote <liuyanoutsee@outlook.com>
2024-10-21 16:59:45 +0800

    【a】待测试

--- samples/src/main/java/samples/SampleApplication.java
+++ samples/src/main/java/samples/SampleApplication.java
@@ -1,5 +1,6 @@
 package samples;
 
+import com.lyflexi.multitenant.config.EnableDynamicDataSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @MapperScan("samples.dao")
 @SpringBootApplication
+@EnableDynamicDataSource
 public class SampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
