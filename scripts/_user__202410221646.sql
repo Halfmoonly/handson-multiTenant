6807c5bee7bd0c4832ec75ffaeda66440c95daf9
lyremote <liuyanoutsee@outlook.com>
2024-10-22 08:11:09 +0800

    [u]更新脚本

--- scripts/_user__202410221646.sql
+++ scripts/_user__202410221646.sql
@@ -1,10 +1,10 @@
 -- tenant1.`user` definition
--- db : tenant1
+-- db : tenant2
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO tenant1.`user` (name) VALUES
-	 ('tenant1');
+INSERT INTO tenant2.`user` (name) VALUES
+	 ('tenant2');
